8ae8bc1c60e89ac8aa72030fd970287e8e8eab5e
biju <unnibiju@gmail.com>
2023-09-01 20:37:57 +0530

    added

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public record Product
+    {
+        readonly string Name;
+
+        readonly string Description;
+
+    }
+}
