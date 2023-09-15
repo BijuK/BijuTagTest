35e239e5bd06579cfdbb57c4a2e733be02091fcb
biju <unnibiju@gmail.com>
2023-09-15 17:45:03 +0530

    ddd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,13 @@ Console.WriteLine(bijuXyz);
 Console.WriteLine(bijuXyz);
 Console.WriteLine(bijuXyz);
 
+var setting = new AppSetting();
 
 
 Console.WriteLine("Hello, World!");
+public class AppSetting 
+{
+    public string? ReferenceKey { get; set; }
+    public string? Value { get; set; }
+    public string? Description { get; set; }
+}
